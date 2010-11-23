c8ea349971611d74131650a1aa60e0d02b584d97
ascentury <ascentury@65838bcf-d22c-c895-750a-766bf6dc372f>
2010-11-23 16:19:37 +0000

    Tagging 1.0.5 version.

---
+++ tags/1.0.5/input.h
@@ -0,0 +1,11 @@
+#ifndef _INPUT_H
+#define	_INPUT_H
+
+void outputHelp();
+void outputVersion();
+void loadRules();
+void loadConfig();
+void parseInput(const int argc, char** argv);
+
+#endif	/* _INPUT_H */
+
