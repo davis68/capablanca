bec4e7311bfafe4e4aa42650a377fcb08631390e
ascentury <ascentury@65838bcf-d22c-c895-750a-766bf6dc372f>
2011-01-21 15:49:39 +0000

    Added -S option to detect full surface if necessary.

--- trunk/src/process.cpp
+++ trunk/src/process.cpp
@@ -27,6 +27,7 @@ using namespace std;
 //  External global variables from global.cpp
 extern bool verbose;
 
+extern bool fullSurface;
 extern uint numStates,
             dissolnStates,
             maxNN;
@@ -177,7 +178,7 @@ void findSurface()
     ParticleMap::iterator mapIter;
     uint numNN;
     //  Stopgap for dealing with alpha-uranium due to the unlinked sheets of nn. FIXME
-    if (maxNN == 4)
+    if (maxNN == 4 || fullSurface)
     {   for (mapIter = pmap.begin(); mapIter != pmap.end(); mapIter++)
         {   if (abs(minZ - mapIter->second.z) > 2*NEIGHBOR_SQUARE_CUTOFF &&
                 abs(maxZ - mapIter->second.z) > 2*NEIGHBOR_SQUARE_CUTOFF &&
